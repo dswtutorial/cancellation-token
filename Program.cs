aa9a141580d6b3fd8a93e4a9c44a2497a43aff0c
Adam Cheriki <private>
2020-07-31 17:23:27 +0300

    Simple Cancellation Example

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancellationTokenExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"\nPlease Enter C to Cancel");
+
+
+            var cancellationToken = new CancellationTokenSource();
+            var task = Task.Run(() =>
+             {
+                 while (true)
+                 {
+                     if (cancellationToken.IsCancellationRequested)
+                     {
+                         Console.WriteLine($"\nCancelled Successfully!");
+                         break;
+                     }
+                     Console.WriteLine($"\nI should stop printing the moment you Cancel...");
+                     Thread.Sleep(1000);
+                 }
+             }, cancellationToken.Token);
+
+            while (true)
+            {
+                if (Char.ToLower(Console.ReadKey().KeyChar) == 'c')
+                {
+                    cancellationToken.Cancel();
+                }
+                else
+                {
+                    Console.WriteLine($"\nSorry :/ I Don't support this input...");
+                }
+
+            }
+        }
+    }
+}
